97008fe5c288a388595b47c24212eb7aa7030778
lucianIrsigler <l.irsigler64@gmail.com>
2024-04-23 20:14:33 +0200

    deploy

--- test.js
+++ test.js
@@ -1,40 +1,10 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://127.0.0.1:3000/compressors=disabled&gssapiServiceName=mongodb';
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:42069/your-database?replicaSet=test-rs';
 
-// Database Name
-const dbName = 'test';
-
-// Create a new user object
-const newUser = {
-    username: 'john',
-    password: 'password123',
-    email: 'john@example.com'
-};
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-    if (err) {
-        console.error('Failed to connect to MongoDB:', err);
-        return;
-    }
-
-    console.log('Connected successfully to MongoDB');
-
-    const db = client.db(dbName);
-
-    // Get the "Users" collection
-    const usersCollection = db.collection('Users');
-
-    // Insert the new user into the "Users" collection
-    usersCollection.insertOne(newUser, function(err, result) {
-        if (err) {
-            console.error('Failed to insert user:', err);
-            return;
-        }
-
-        console.log('User added successfully:', result.ops[0]);
-        client.close();
-    });
-});
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
